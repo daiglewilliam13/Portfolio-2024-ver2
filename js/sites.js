26a6fe106c45628c0f831629bb16478c41ea00b4
William Daigle <daiglewilliam13@gmail.com>
2024-09-04 15:37:01 -0700

    add works

---
+++ js/sites.js
@@ -0,0 +1,19 @@
+const works = [{
+    title:"TsumiCon",
+    type:"Event",
+    url: "https://www.tsumicon.com",
+    tech: ['JavaScript', 'Front-End', 'REST APIs', 'Wix']
+},
+{
+    title:"Bug Tracker",
+    type:"Tooling",
+    url: "TBD",
+    tech: ['TypeScript', 'React', 'REST APIs', 'NextJS']
+},
+{
+    title:"Great Smiles Vegas Valley",
+    type:"Brochure",
+    url: "TBD",
+    tech: ['Front-End', 'Wordpress', 'JavaScript']
+},
+]
